c5198131bb07343fd6f2f8a0e358aa58d246f3f7
Brendan Healy <brendanehealy@gmail.com>
2025-08-08 17:04:19 -0600

    get rid of module local models

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,2 +1,25 @@
+from tkinter.tix import TEXT
 from .opresult import *
-from .requests import *
+
+# 0 -- openpose
+# 1 -- depth
+# 2 -- hed/pidi/scribble/ted
+# 3 -- canny/lineart/anime_lineart/mlsd
+# 4 -- normal
+# 5 -- segment
+# 6 -- tile
+# 7 -- repaint
+class CNUnionControlMode(Enum):
+    OPENPOSE = 0
+    DEPTH = 1
+    HED_PIDI_SCRIBBLE_TED = 2
+    CANNY_LINEART_ANIME_LINEART_MLSD = 3
+    NORMAL = 4
+    SEGMENT = 5
+    TILE = 6
+    INPAINT = 7
+
+class PipeType(Enum):
+    TEXT2IMGE = 1
+    IMAGE2IMAGE = 2
+    INPAINT = 3
